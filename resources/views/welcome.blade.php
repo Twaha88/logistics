2a26a187f4eb0696b66a70e7df2058e6bed84a44
Twaha88 <twahakassim88@gmail.com>
2023-11-01 18:22:24 +0300

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,48 @@
+<head>
+    <title>Login Page</title>
+    <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js">
+    </script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins&display=swap">
+    </head>
+      <body> <div class="container col-md-3" style="padding-top: 100px;">
+        <div class="card">
+           <div class="card-header"
+           style="font-family: Nunito;" >
+            <h4 style="text-align: center;" ><b> LOGISTICS </b></h4>
+
+        </div>
+        <div class="card-body">
+            <form> <div class="form-group">
+                <label for="email">Username:</label>
+                 <input type="email" class="form-control" id="email" placeholder="Enter Username" name="email"> </div>
+                  <div class="form-group"> <label for="pwd">Password:</label>
+                    <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd"> </div>
+                     <div class="form-group form-check">
+                         <label class="form-check-label">
+
+                         </div>
+                         <div class="text-center ">
+
+                            <button type="button" class="btn btn-primary col-md-4" style="font-family: Poppins; border-radius:20px;" >LOGIN</button> </form>
+                    </div>
+
+
+
+                </div>
+
+
+        </div>
+
+
+
+
+        </div>
+
+        </body>
+    </html>
